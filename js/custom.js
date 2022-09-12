bd05e6bb4112c1814cf0d6e9eb538af414bfac60
Chiullian <Chiullian@163.com>
2022-09-12 22:57:00 +0800

    Site updated: 2022-09-12 22:56:58

---
+++ js/custom.js
@@ -0,0 +1,211 @@
+// 卡片收起来
+var card_category_list = document.getElementsByClassName(
+    "card-category-list child"
+);
+var item = document.getElementsByClassName("card-category-list-item");
+function toggle(t) {
+    var display = t.parentElement.nextSibling.style.display;
+    if (display == "none") {
+        t.parentElement.nextSibling.style.display = "block";
+        t.parentElement.nextSibling.style.height = "100%";
+        t.className = t.className.replace("fa-chevron-up", "fa-chevron-down");
+    } else {
+        t.parentElement.nextSibling.style.display = "none";
+        t.className = t.className.replace("fa-chevron-down", "fa-chevron-up");
+    }
+}
+
+for (var i = 0; i < card_category_list.length; i++) {
+    card_category_list[i].style.display = "none";
+    card_category_list[i].style.transition = "all 1s";
+    card_category_list[i].previousSibling.innerHTML +=
+        '<i class="fa fa-chevron-up menus-expand  menus-closed" aria-hidden="true" style="margin-left:20px;" οnclick="toggle(this)"></i>';
+}
+
+/*切换图片背景-----------------------------------------*/
+// 存数据
+// name：命名 data：数据
+function saveData(name, data) {
+    localStorage.setItem(name, JSON.stringify({ 'time': Date.now(), 'data': data }))
+}
+
+// 取数据
+// name：命名 time：过期时长,单位分钟,如传入30,即加载数据时如果超出30分钟返回0,否则返回数据
+function loadData(name, time) {
+    let d = JSON.parse(localStorage.getItem(name));
+    // 过期或有错误返回 0 否则返回数据
+    if (d) {
+        let t = Date.now() - d.time
+        if (t < (time * 60 * 1000) && t > -1) return d.data;
+    }
+    return 0;
+}
+
+// 上面两个函数如果你有其他需要存取数据的功能，也可以直接使用
+
+// 读取背景
+try {
+    let data = loadData('blogbg', 1440)
+    if (data) changeBg(data, 1)
+    else localStorage.removeItem('blogbg');
+} catch (error) { localStorage.removeItem('blogbg'); }
+
+// 切换背景函数
+// 此处的flag是为了每次读取时都重新存储一次,导致过期时间不稳定
+// 如果flag为0则存储,即设置背景. 为1则不存储,即每次加载自动读取背景.
+function changeBg(s, flag) {
+    let bg = document.getElementById('web_bg')
+    if (s.charAt(0) == '#') {
+        bg.style.backgroundColor = s
+        bg.style.backgroundImage = 'none'
+    } else bg.style.backgroundImage = s
+    if (!flag) { saveData('blogbg', s) }
+}
+
+/*   博客底部运行时间 ---------------------------------- */
+setInterval(() => {
+    // let create_time = Math.round(new Date('2021-10-15 00:00:00').getTime() / 1000); //在此行修改建站时间
+    // 有苹果用户发现safari浏览器不能正常运行，百度了一下发现是格式化的问题，改成下面这种应该就可以了。感谢反馈。
+    let create_time = Math.round(new Date('2021/10/15 00:00:00').getTime() / 1000); //在此行修改建站时间
+    let timestamp = Math.round((new Date().getTime()) / 1000);
+    let second = timestamp - create_time;
+    let time = new Array(0, 0, 0, 0, 0);
+
+    var nol = function(h) {
+        return h > 9 ? h : '0' + h;
+    }
+    if (second >= 365 * 24 * 3600) {
+        time[0] = parseInt(second / (365 * 24 * 3600));
+        second %= 365 * 24 * 3600;
+    }
+    if (second >= 24 * 3600) {
+        time[1] = parseInt(second / (24 * 3600));
+        second %= 24 * 3600;
+    }
+    if (second >= 3600) {
+        time[2] = nol(parseInt(second / 3600));
+        second %= 3600;
+    }
+    if (second >= 60) {
+        time[3] = nol(parseInt(second / 60));
+        second %= 60;
+    }
+    if (second >= 0) {
+        time[4] = nol(second);
+    }
+    let currentTimeHtml = ""
+    if (time[0] != 0) {
+        currentTimeHtml += time[0] + ' YEAR '
+    }
+    currentTimeHtml += time[1] + ' DAYS ' + time[2] + ' : ' + time[3] + ' : ' + time[4];
+    document.getElementById("runtime").innerHTML = currentTimeHtml;
+}, 1000);
+
+
+/*选择壁纸----------------------------------------*/
+// 存数据
+// name：命名 data：数据
+function saveData(name, data) {
+    localStorage.setItem(name, JSON.stringify({ 'time': Date.now(), 'data': data }))
+}
+
+// 取数据
+// name：命名 time：过期时长,单位分钟,如传入30,即加载数据时如果超出30分钟返回0,否则返回数据
+function loadData(name, time) {
+    let d = JSON.parse(localStorage.getItem(name));
+    // 过期或有错误返回 0 否则返回数据
+    if (d) {
+        let t = Date.now() - d.time
+        if (t < (time * 60 * 1000) && t > -1) return d.data;
+    }
+    return 0;
+}
+
+// 上面两个函数如果你有其他需要存取数据的功能，也可以直接使用
+
+// 读取背景
+try {
+    let data = loadData('blogbg', 1440)
+    if (data) changeBg(data, 1)
+    else localStorage.removeItem('blogbg');
+} catch (error) { localStorage.removeItem('blogbg'); }
+
+// 切换背景函数
+// 此处的flag是为了每次读取时都重新存储一次,导致过期时间不稳定
+// 如果flag为0则存储,即设置背景. 为1则不存储,即每次加载自动读取背景.
+function changeBg(s, flag) {
+    let bg = document.getElementById('web_bg')
+    if (s.charAt(0) == '#') {
+        bg.style.backgroundColor = s
+        bg.style.backgroundImage = 'none'
+    } else bg.style.backgroundImage = s
+    if (!flag) { saveData('blogbg', s) }
+}
+
+// 以下为2.0新增内容
+
+// 创建窗口
+var winbox = ''
+
+function createWinbox() {
+    let div = document.createElement('div')
+    document.body.appendChild(div)
+    winbox = WinBox({
+        id: 'changeBgBox',
+        index: 999,
+        title: "切换背景",
+        x: "center",
+        y: "center",
+        minwidth: '300px',
+        height: "60%",
+        background: 'var(--leonus-blue)',
+        onmaximize: () => { div.innerHTML = `<style>body::-webkit-scrollbar {display: none;}div#changeBgBox {width: 100% !important;}</style>` },
+        onrestore: () => { div.innerHTML = '' }
+    });
+    winResize();
+    window.addEventListener('resize', winResize)
+
+    // 每一类我放了一个演示，直接往下复制粘贴 a标签 就可以，需要注意的是 函数里面的链接 冒号前面需要添加反斜杠\进行转义
+    winbox.body.innerHTML = `
+    <div id="article-container" style="padding:10px;">
+    
+    <p><button onclick="localStorage.removeItem('blogbg');location.reload();" style="background:#5fcdff;display:block;width:100%;padding: 15px 0;border-radius:6px;color:white;"><i class="fa-solid fa-arrows-rotate"></i> 点我恢复默认背景</button></p>
+    <h2 id="图片（手机）"><a href="#图片（手机）" class="headerlink" title="图片（手机）"></a>图片（手机）</h2>
+    <div class="bgbox">
+    <a href="javascript:;" rel="noopener external nofollow" style="background-image:url(https://img.vm.laomishuo.com/image/2021/12/2021122715170589.jpeg)" class="pimgbox" onclick="changeBg('url(https\://img.vm.laomishuo.com/image/2021/12/2021122715170589.jpeg)')"></a>
+    </div>
+    
+    <h2 id="图片（电脑）"><a href="#图片（电脑）" class="headerlink" title="图片（电脑）"></a>图片（电脑）</h2>
+    <div class="bgbox">
+    <a href="javascript:;" rel="noopener external nofollow" style="background-image:url(https://cn.bing.com/th?id=OHR.GBRTurtle_ZH-CN6069093254_1920x1080.jpg)" class="imgbox" onclick="changeBg('url(https\://cn.bing.com/th?id=OHR.GBRTurtle_ZH-CN6069093254_1920x1080.jpg)')"></a>
+    </div>
+    
+    
+    
+    <h2 id="渐变色"><a href="#渐变色" class="headerlink" title="渐变色"></a>渐变色</h2>
+    <div class="bgbox">
+    <a href="javascript:;" rel="noopener external nofollow" class="box" style="background: linear-gradient(to right, #eecda3, #ef629f)" onclick="changeBg('linear-gradient(to right, #eecda3, #ef629f)')"></a>
+    </div>
+    
+    <h2 id="纯色"><a href="#纯色" class="headerlink" title="纯色"></a>纯色</h2>
+    <div class="bgbox">
+    <a href="javascript:;" rel="noopener external nofollow" class="box" style="background: #7D9D9C" onclick="changeBg('#7D9D9C')"></a> 
+    </div>
+`;
+}
+
+// 适应窗口大小
+function winResize() {
+    var offsetWid = document.documentElement.clientWidth;
+    if (offsetWid <= 768) {
+        winbox.resize(offsetWid * 0.95 + "px", "90%").move("center", "center");
+    } else {
+        winbox.resize(offsetWid * 0.6 + "px", "70%").move("center", "center");
+    }
+}
+
+// 切换状态，窗口已创建则控制窗口显示和隐藏，没窗口则创建窗口
+function toggleWinbox() {
+    if (document.querySelector('#changeBgBox')) winbox.toggleClass('hide');
+    else createWinbox();
+}
