e0d2ecf57f61e053a50ed9a7e0f8ac1cd519c525
Chiullian <Chiullian@163.com>
2022-09-23 12:19:18 +0800

    Site updated: 2022-09-23 12:19:17

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,104 @@
-var card_category_list=document.getElementsByClassName("card-category-list child"),item=document.getElementsByClassName("card-category-list-item");function toggle(e){"none"==e.parentElement.nextSibling.style.display?(e.parentElement.nextSibling.style.display="block",e.parentElement.nextSibling.style.height="100%",e.className=e.className.replace("fa-chevron-up","fa-chevron-down")):(e.parentElement.nextSibling.style.display="none",e.className=e.className.replace("fa-chevron-down","fa-chevron-up"))}for(var i=0;i<card_category_list.length;i++)card_category_list[i].style.display="none",card_category_list[i].style.transition="all 1s",card_category_list[i].previousSibling.innerHTML+='<i class="fa fa-chevron-up menus-expand  menus-closed" aria-hidden="true" style="margin-left:20px;" οnclick="toggle(this)"></i>';function saveData(e,t){localStorage.setItem(e,JSON.stringify({time:Date.now(),data:t}))}function loadData(e,t){let a=JSON.parse(localStorage.getItem(e));if(a){let e=Date.now()-a.time;if(e<60*t*1e3&&e>-1)return a.data}return 0}try{let e=loadData("blogbg",1440);e?changeBg(e,1):localStorage.removeItem("blogbg")}catch(e){localStorage.removeItem("blogbg")}function changeBg(e,t){let a=document.getElementById("web_bg");"#"==e.charAt(0)?(a.style.backgroundColor=e,a.style.backgroundImage="none"):a.style.backgroundImage=e,t||saveData("blogbg",e)}setInterval((()=>{let e=Math.round(new Date("2021/10/15 00:00:00").getTime()/1e3),t=Math.round((new Date).getTime()/1e3)-e,a=new Array(0,0,0,0,0);var n=function(e){return e>9?e:"0"+e};t>=31536e3&&(a[0]=parseInt(t/31536e3),t%=31536e3),t>=86400&&(a[1]=parseInt(t/86400),t%=86400),t>=3600&&(a[2]=n(parseInt(t/3600)),t%=3600),t>=60&&(a[3]=n(parseInt(t/60)),t%=60),t>=0&&(a[4]=n(t));let l="";0!=a[0]&&(l+=a[0]+" YEAR "),l+=a[1]+" DAYS "+a[2]+" : "+a[3]+" : "+a[4],document.getElementById("runtime").innerHTML=l}),1e3);
+// 卡片收起来
+var card_category_list = document.getElementsByClassName(
+    "card-category-list child"
+);
+var item = document.getElementsByClassName("card-category-list-item");
+function toggle(t) {
+    var display = t.parentElement.nextSibling.style.display;
+    if (display == "none") {
+        t.parentElement.nextSibling.style.display = "block";
+        t.parentElement.nextSibling.style.height = "100%";
+        t.className = t.className.replace("fa-chevron-up", "fa-chevron-down");
+    } else {
+        t.parentElement.nextSibling.style.display = "none";
+        t.className = t.className.replace("fa-chevron-down", "fa-chevron-up");
+    }
+}
+
+for (var i = 0; i < card_category_list.length; i++) {
+    card_category_list[i].style.display = "none";
+    card_category_list[i].style.transition = "all 1s";
+    card_category_list[i].previousSibling.innerHTML +=
+        '<i class="fa fa-chevron-up menus-expand  menus-closed" aria-hidden="true" style="margin-left:20px;" οnclick="toggle(this)"></i>';
+}
+
+/*切换图片背景-----------------------------------------*/
+// 存数据
+// name：命名 data：数据
+function saveData(name, data) {
+    localStorage.setItem(name, JSON.stringify({ 'time': Date.now(), 'data': data }))
+}
+
+// 取数据
+// name：命名 time：过期时长,单位分钟,如传入30,即加载数据时如果超出30分钟返回0,否则返回数据
+function loadData(name, time) {
+    let d = JSON.parse(localStorage.getItem(name));
+    // 过期或有错误返回 0 否则返回数据
+    if (d) {
+        let t = Date.now() - d.time
+        if (t < (time * 60 * 1000) && t > -1) return d.data;
+    }
+    return 0;
+}
+
+// 上面两个函数如果你有其他需要存取数据的功能，也可以直接使用
+
+// 读取背景
+try {
+    let data = loadData('blogbg', 1440)
+    if (data) changeBg(data, 1)
+    else localStorage.removeItem('blogbg');
+} catch (error) { localStorage.removeItem('blogbg'); }
+
+// 切换背景函数
+// 此处的flag是为了每次读取时都重新存储一次,导致过期时间不稳定
+// 如果flag为0则存储,即设置背景. 为1则不存储,即每次加载自动读取背景.
+function changeBg(s, flag) {
+    let bg = document.getElementById('web_bg')
+    if (s.charAt(0) == '#') {
+        bg.style.backgroundColor = s
+        bg.style.backgroundImage = 'none'
+    } else bg.style.backgroundImage = s
+    if (!flag) { saveData('blogbg', s) }
+}
+
+/*   博客底部运行时间 ---------------------------------- */
+setInterval(() => {
+    // let create_time = Math.round(new Date('2021-10-15 00:00:00').getTime() / 1000); //在此行修改建站时间
+    // 有苹果用户发现safari浏览器不能正常运行，百度了一下发现是格式化的问题，改成下面这种应该就可以了。感谢反馈。
+    let create_time = Math.round(new Date('2021/10/15 00:00:00').getTime() / 1000); //在此行修改建站时间
+    let timestamp = Math.round((new Date().getTime()) / 1000);
+    let second = timestamp - create_time;
+    let time = new Array(0, 0, 0, 0, 0);
+
+    var nol = function(h) {
+        return h > 9 ? h : '0' + h;
+    }
+    if (second >= 365 * 24 * 3600) {
+        time[0] = parseInt(second / (365 * 24 * 3600));
+        second %= 365 * 24 * 3600;
+    }
+    if (second >= 24 * 3600) {
+        time[1] = parseInt(second / (24 * 3600));
+        second %= 24 * 3600;
+    }
+    if (second >= 3600) {
+        time[2] = nol(parseInt(second / 3600));
+        second %= 3600;
+    }
+    if (second >= 60) {
+        time[3] = nol(parseInt(second / 60));
+        second %= 60;
+    }
+    if (second >= 0) {
+        time[4] = nol(second);
+    }
+    let currentTimeHtml = ""
+    if (time[0] != 0) {
+        currentTimeHtml += time[0] + ' YEAR '
+    }
+    currentTimeHtml += time[1] + ' DAYS ' + time[2] + ' : ' + time[3] + ' : ' + time[4];
+    document.getElementById("runtime").innerHTML = currentTimeHtml;
+}, 1000);
+
+
